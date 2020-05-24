3e64477e062655584a3590128748318d5dc2f7ba
Valery3dm <jew.3dm@gmail.com>
2020-05-24 17:32:39 +0300

    workBurger

---
+++ scripts/burger.js
@@ -0,0 +1,6 @@
+document.getElementById('burgerImg')
+    .addEventListener('click',burger);
+function burger() {
+    const classList = document.querySelector('.active').classList.toggle('hidden');   
+}
+
