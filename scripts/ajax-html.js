af638ad370bbee43a6d6df0ec8f68e4bb1f20aea
Valery3dm <jew.3dm@gmail.com>
2020-06-03 23:52:50 +0300

    ajaxTest7

--- scripts/ajax-html.js
+++ scripts/ajax-html.js
@@ -61,39 +61,21 @@
         document.querySelector('.designLess').classList.toggle('designDel'); 
     } */
 
-    document.querySelectorAll('#but')
-	    .forEach(element => element.addEventListener('click', getAjaxElement));
+document.querySelectorAll('#but')
+	.forEach(element => element.addEventListener('click', getAjaxElement));
 
-function getAjaxElement(e) {
-	let clickedButton = e.target.value;
-	const xhr = new XMLHttpRequest();
-	let past;
-	let openXHR;
-	let changeClass;
-	if (clickedButton == "rhino") {
-		past = document.getElementById('get-html-rhino');
-		openXHR = xhr.open('GET', 'form/rhino-form.html', true);
-		changeClass = document.querySelector('.rhinoLess').classList.toggle('rhinoDel');
-	} else if (clickedButton == "zbrush") {
-		past = document.getElementById('get-html-zbrush');
-		openXHR = xhr.open('GET', 'form/zbrush-html.html', true);
-		changeClass = document.querySelector('.zbrushLess').classList.toggle('zbrushDel');
-	} else if (clickedButton == "matrix") {
-		past = document.getElementById('get-html-matrix');
-		openXHR = xhr.open('GET', 'form/matrix-html.html', true);
-		changeClass = document.querySelector('.matrixLess').classList.toggle('matrixDel');
-	} else {
-		past = document.getElementById('get-html-design');
-		openXHR = xhr.open('GET', 'form/design-html.html', true);
-		changeClass = document.querySelector('.designLess').classList.toggle('designDel');
-	}
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState === 4 && xhr.status === 200) {
-			past.innerHTML = xhr.responseText;
-		}
-	}
-	openXHR;
-	xhr.send();
-	changeClass;
-
+    function getAjaxElement(e) {
+    
+	const clickedButton = e.target.value;
+    const xhr = new XMLHttpRequest();
+    
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById(`get-html-${clickedButton}`)
+                .innerHTML = xhr.responseText;
+        } 
+    }
+    xhr.open('GET',`form/${clickedButton}-form.html`,true);
+    xhr.send();
+    document.querySelector(`.${clickedButton}Less`).classList.toggle(`${clickedButton}Del`); 
 }
