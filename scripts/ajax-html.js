3602fac4ef9dd05723606ac32d1c9377ceabe8c8
Valery3dm <jew.3dm@gmail.com>
2020-06-05 19:03:18 +0300

    cssTest2

--- scripts/ajax-html.js
+++ scripts/ajax-html.js
@@ -1,65 +1,4 @@
-
-    /* document.getElementById('rhino')
-        .addEventListener('click',rhino);
-    document.getElementById('zbrush')
-        .addEventListener('click',zbrush);
-    document.getElementById('matrix')
-        .addEventListener('click',matrix);
-    document.getElementById('design')
-        .addEventListener('click',design);
-
-    function rhino(){
-        const xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                document.getElementById('get-html-rhino')
-                    .innerHTML = xhr.responseText;
-            } 
-        }
-        xhr.open('GET','form/rhino-form.html',true);
-        xhr.send();
-        document.querySelector('.rhinoLess').classList.toggle('rhinoDel');  
-    }
-
-    function zbrush(){
-        const xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                document.getElementById('get-html-zbrush')
-                    .innerHTML = xhr.responseText;
-            } 
-        }
-        xhr.open('GET','form/zbrush-html.html',true);
-        xhr.send();
-        document.querySelector('.zbrushLess').classList.toggle('zbrushDel'); 
-    }
-
-
-    function matrix(){
-        const xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                document.getElementById('get-html-matrix')
-                    .innerHTML = xhr.responseText;
-            } 
-        }
-        xhr.open('GET','form/matrix-html.html',true);
-        xhr.send();
-        document.querySelector('.matrixLess').classList.toggle('matrixDel'); 
-    }
-
-    function design(){
-        const xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                document.getElementById('get-html-design')
-                    .innerHTML = xhr.responseText;
-            } 
-        }
-        xhr.open('GET','form/design-html.html',true);
-        xhr.send();
-        document.querySelector('.designLess').classList.toggle('designDel'); 
-    } */
+(function () { 
 
 document.querySelectorAll('#but')
 	.forEach(element => element.addEventListener('click', getAjaxElement));
@@ -79,3 +18,5 @@ document.querySelectorAll('#but')
     xhr.send();
     document.querySelector(`.${clickedButton}Less`).classList.toggle(`${clickedButton}Del`); 
 }
+
+}());
